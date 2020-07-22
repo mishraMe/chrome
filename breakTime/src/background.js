ee7976502696981b7316ec4838bae3a0896bc797
memishra <minakshi.mishra.us@gmail.com>
2020-07-22 00:08:28 -0400

    switch now changes and shows ON/OFF, resloved multiple tabs open issue.

--- breakTime/src/background.js
+++ breakTime/src/background.js
@@ -1,17 +1,28 @@
+chrome.runtime.onInstalled.addListener(function(){
+    chrome.storage.sync.set({toggl: false}, function () {
+        console.log("Installed with the bydefault value of false");
+        togglState = false;
+    });
+});
 
 //this event fires everytime any of the storage values is changed. in our case toggl button.
 chrome.storage.onChanged.addListener(function (changes, sync) {
+    console.log("a storage value changed");
     for (key in changes){
-        var keyVal = changes[key]; // returns boolean if toggl button
-       if(key === 'toggl' && keyVal){
-           chrome.alarms.create("break", {delayInMinutes: 10, periodInMinutes: 30})
+        let keyVal = changes[key]; // returns boolean if toggl button
+       if(key === 'toggl' && keyVal.newValue){
+           chrome.alarms.create("break", {periodInMinutes: 1});
+       }
+       if(key === 'toggl' && !keyVal.newValue){
+           console.log("clearing this alarm");
+           chrome.alarms.clear("break");
        }
     }
-    //this event fires an alarm everytime the time elapses as decided on the alarm creation.
+});
+
+ //this event fires an alarm everytime the time elapses as decided on the alarm creation.
     chrome.alarms.onAlarm.addListener(function(alarm){
-        console.log("Got an alarm!", alarm);
         chrome.tabs.create({url: 'https://www.youtube.com/watch?v=dkedupX73xs', active: true}, function (tab) {
             console.log("tab info for the tab just opened is " + tab);
         })
-    })
-});
+    });
--- breakTime/src/switch.js
+++ breakTime/src/switch.js
@@ -1,14 +1,14 @@
-let togglState;
-chrome.storage.sync.set({toggl: false}, function () {
-    console.log("I am called only once");
-    togglState = false;
-});
-toggleSwitch.onclick = function toggeTheSwitch () {
-    chrome.storage.sync.get(['toggl'], function(data){
-        togglState = data.toggl;
-    });
-    chrome.storage.sync.set({toggl: !togglState}, function () {
-        console.log("setting new value of toggl in storage to " + !togglState);
-    });
+let breakTimeSwitch = document.getElementById('toggleSwitch');
 
-};
+chrome.storage.sync.get('toggl', function(data){
+console.log(data.toggl);
+breakTimeSwitch.innerHTML = data.toggl ? 'ON' : 'OFF';
+});
+
+breakTimeSwitch.onclick = function(element){
+let nextState = element.target.innerHTML === 'ON' ? false : true;
+chrome.storage.sync.set({'toggl' : nextState}, function(){
+    console.log("the switch was set to : " + nextState);
+    });
+    window.close();
+}
