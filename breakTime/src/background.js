d19bf6500dc4f1d88b8fc1f8354f1b29643c6e2a
memishra <minakshi.mishra.us@gmail.com>
2020-08-11 01:32:40 -0400

    added logic for alarm reset and closing tab after break

--- breakTime/src/background.js
+++ breakTime/src/background.js
@@ -1,3 +1,4 @@
+
 chrome.runtime.onInstalled.addListener(function(){
     chrome.storage.sync.set({toggl: false}, function () {
         console.log("Installed with the bydefault value of false");
@@ -6,23 +7,37 @@ chrome.runtime.onInstalled.addListener(function(){
 });
 
 //this event fires everytime any of the storage values is changed. in our case toggl button.
+//we need a global variable that can be modified whenever the script needs to modify next Alarm time
+
 chrome.storage.onChanged.addListener(function (changes, sync) {
-    console.log("a storage value changed");
     for (key in changes){
         let keyVal = changes[key]; // returns boolean if toggl button
        if(key === 'toggl' && keyVal.newValue){
-           chrome.alarms.create("break", {periodInMinutes: 1});
+           chrome.alarms.create("break", {delayInMinutes: 30});
        }
        if(key === 'toggl' && !keyVal.newValue){
            console.log("clearing this alarm");
-           chrome.alarms.clear("break");
+           chrome.alarms.clearAll();
        }
     }
 });
 
- //this event fires an alarm everytime the time elapses as decided on the alarm creation.
+ //this event fires an alarm every time the time elapses as decided on the alarm creation.
     chrome.alarms.onAlarm.addListener(function(alarm){
-        chrome.tabs.create({url: 'https://www.youtube.com/watch?v=dkedupX73xs', active: true}, function (tab) {
-            console.log("tab info for the tab just opened is " + tab);
-        })
+        if(alarm.name === "break"){
+            chrome.tabs.create({url: 'https://www.youtube.com/watch?v=dkedupX73xs', active: true}, function (tab){
+                chrome.storage.sync.set({breakTabId: tab.id});
+            });
+            chrome.alarms.create("breakOver", {delayInMinutes : 10});
+        }
+
+        if(alarm.name === "breakOver"){
+            chrome.storage.sync.get(['breakTabId'],
+             function(data){
+                chrome.tabs.remove(data.breakTabId);
+            });
+            chrome.storage.sync.remove("breakTabId");
+            chrome.alarms.create("break", {delayInMinutes: 30});
+        }
+
     });
